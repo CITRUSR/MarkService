344da13b8715c8d1bc5ed1d4200b9bdb82889ec4
CITRUSR <savelypetrov2007@mail.ru>
2025-03-15 23:42:22 +0300

    add student service

--- MarkService.Infrastructure/DependencyInjection.cs
+++ MarkService.Infrastructure/DependencyInjection.cs
@@ -1,3 +1,5 @@
+using MarkService.Application.Contracts.ExternalServices.Student;
+using MarkService.Infrastructure.ExternalServices.UserService.Student;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,6 +12,47 @@ public static class DependencyInjection
         IConfiguration configuration
     )
     {
+        AddGrpcClients(services, configuration);
+        AddExternalServices(services);
         return services;
+    }
+
+    private static void AddGrpcClients(IServiceCollection services, IConfiguration configuration)
+    {
+        var badCertHttpClientHandler = new HttpClientHandler();
+
+        badCertHttpClientHandler.ServerCertificateCustomValidationCallback =
+            HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+        AddUsersApi(services, configuration, badCertHttpClientHandler);
+    }
+
+    private static void AddUsersApi(
+        IServiceCollection services,
+        IConfiguration configuration,
+        HttpClientHandler? httpClientHandler = null
+    )
+    {
+        var apiUrl = configuration["MicroServices:UsersAPI"];
+
+        if (string.IsNullOrEmpty(apiUrl))
+        {
+            throw new InvalidOperationException("userAPI url is not set");
+        }
+
+        services
+            .AddGrpcClient<UserServiceClient.StudentService.StudentServiceClient>(cfg =>
+            {
+                cfg.Address = new Uri(apiUrl);
+            })
+            .ConfigurePrimaryHttpMessageHandler(() =>
+            {
+                return httpClientHandler ?? new HttpClientHandler();
+            });
+    }
+
+    private static void AddExternalServices(IServiceCollection services)
+    {
+        services.AddScoped<IStudentService, StudentService>();
     }
 }
